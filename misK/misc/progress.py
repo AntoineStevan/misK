4383e30a2bc74a62ce96aba7dbe6530aa1b58df3
Antoine Stevan <stevan.antoine@gmail.com>
2021-06-29 12:23:01 +0200

    ProgressBar class.

--- misK/misc/progress.py
+++ misK/misc/progress.py
@@ -1,4 +1,6 @@
-# import time
+import time
+
+
 #
 # bars = "\\|/-"
 #
@@ -16,14 +18,66 @@
 #     print('\r', frame, sep='', end='', flush=True)
 #     sleep(0.2)
 
+class ProgressBar:
+    def __init__(self, total, width=100):
+        self.total = total
+        self.progress = 0
+        self.width = width
+        self.formats = ["{:5.1f}%",
+                        "{:" + str(len(str(self.total))) + "d}/{}",
+                        "[{:02d}:{:02d}<{:02d}:{:02d}, {:4.2f} {}]"]
 
-def progress_bar(current, total, width=100):
-    progress = 100 * current // (total-1)
-    done = progress * width // 100
-    print('\r[', '#' * done, ' ' * (width - done), ']', f" {progress:.0f}%", sep='', end='')
+        self.start = time.time()
+        self.times = {}
+
+    def _bar(self):
+        progress = 100 * self.progress / self.total
+        bar_format = ' '.join([self.formats[0], self._inner_bar(progress), self.formats[1], self.formats[2]])
+        t = time.time() - self.start
+        self.times[self.progress] = t
+        pred = int(t * self.total / self.progress - t)
+        t = int(t)
+        speed = list(self.times.values())[-10:]
+        speed = (speed[-1] - speed[0]) / len(speed)
+        speed, it = (speed, "its/s") if speed >= 1 else (1/speed, "s/it") if speed > 0 else (float("nan"), "----")
+        return bar_format.format(progress, self.progress, self.total,
+                                 t // 60, t % 60, pred // 60, pred % 60,
+                                 speed, it)
+
+    def __call__(self, incr=1, force=None):
+        if force is None:
+            self.progress += incr
+        else:
+            self.progress = force
+
+        print('\r', self._bar(), sep='', end='')
+
+    def _inner_bar(self, progress):
+        done = int(progress * self.width / 100)
+        digit = int(round((progress - int(progress)) * 10, 0))
+        bar = '#' * done + str('#' if digit == 10 else digit) + ' ' * (self.width - done - 1)
+        return '[' + bar[:self.width] + ']'
+
+    def close(self):
+        print('\r', self._bar(), sep='')
 
 
 if __name__ == "__main__":
-    n = 100000
+    from tqdm import tqdm, trange
+    from time import sleep
+
+    # with tqdm(total=100, ascii=True) as pbar:
+    #     for i in range(100):
+    #         sleep(0.1)
+    #         pbar.update(1)
+
+    # for i in trange(3, desc='1st loop'):
+    #     for j in tqdm(range(100), desc='2nd loop', leave=False):
+    #         sleep(0.01)
+
+    n = 5123
+    bar = ProgressBar(n, width=100)
     for i in range(n):
-        progress_bar(i, n)
+        sleep(0.1)
+        bar(incr=1)
+    bar.close()
